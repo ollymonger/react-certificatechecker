2e9435b5cd7bba8af25c54f9dcbdbc47c1ebd197
Olly Monger <olly@yllo.co.uk>
2019-11-13 17:02:15 +0000

    /getManualChecks - returns all Manual Checks from the Database!

--- server/server.ts
+++ server/server.ts
@@ -31,10 +31,59 @@ app.get('/', function (req, res) {
     res.send('Cookies: ' + JSON.stringify(req.cookies));
 })
 
+
 app.get('/lastRefreshed', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(lastRefreshed);
 })
+
+
+app.get('/getManualChecks', function(req, res) {
+    var query = new storage.TableQuery().top(100).where('PartitionKey eq ?', 'ManualUrl') //ROWKEY MUST EQUAL MANUAL
+    storageClient.queryEntities('urls', query, null, { payloadFormat: "application/json;odata=nometadata" }, function (err, resu, resp) {
+        if(!err) {
+            var manualChecks = resu.entries;
+            var jsonResult = [];
+
+            for(var i = 0; i < manualChecks.length; i++) {
+                var jsonEntries = {
+                    name: resu.entries[i].Url._,
+                    valid: resu.entries[i].Valid._,
+                    valid_to: resu.entries[i].Valid_to._,
+                    description: resu.entries[i].Desc._
+                }
+                jsonResult.push(jsonEntries);
+            }
+            res.send(jsonResult)
+        } else {
+            console.error(err);
+            res.send("Invalid request")
+        }
+    })}
+)
+
+app.get('/getManualChecks', function(req, res) {
+    var query = new storage.TableQuery().select('RowKey eq ?', 'ManualCheck') //ROWKEY MUST EQUAL MANUAL
+    storageClient.queryEntities('urls', query, null, { payloadFormat: "application/json;odata=nometadata" }, function (err, resu, resp) {
+        if(!err) {
+            var manualChecks = resu.entries;
+            var jsonResult = [];
+
+            for(var i = 0; i < manualChecks.length; i++) {
+                var jsonEntries = {
+                    name: resu.entries[i].Url._,
+                    valid: resu.entries[i].Valid._,
+                    valid_to: resu.entries[i].Valid_to._,
+                    description: resu.entries[i].Desc._
+                }
+                jsonResult.push(jsonEntries);
+            }
+        } else {
+            console.error(err);
+            res.send("Invalid request")
+        }
+    })}
+)
 
 app.get('/checkCerts', function (req, res) {
     var array = [];
